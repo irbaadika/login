f7ab83f3eb5d5c6b38221ae4120ced39f8083778
irbaadika <irbaadika123@gmail.com>
2023-05-30 19:59:51 +0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 255, 0, 0);
+const kPrimaryLightColor = Color.fromARGB(255, 199, 199, 199);
+
+const double defaultPadding = 16.0;
